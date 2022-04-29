bb71cc6e8480081b8c982363e82b1e2efecd8cf2
elaheerazob <ridwanelahee282@gmail.com>
2022-04-29 14:48:01 +0600

    pages add

---
+++ src/Pages/Car/Car.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Car = () => {
+    return (
+        <div>
+            <h2>car</h2>
+        </div>
+    );
+};
+
+export default Car;
---
+++ src/Pages/CarAddForm/CarAddForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CarAddForm = () => {
+    return (
+        <div>
+            <h2>Car Add form</h2>
+        </div>
+    );
+};
+
+export default CarAddForm;
