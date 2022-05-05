7b11107dcfb92e17837165230704aae0889b5516
elaheerazob <ridwanelahee282@gmail.com>
2022-05-05 15:12:15 +0600

    authentication set email password

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,41 +1,99 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 import "./Login.css";
+import auth from "../../../firebase.init";
+import Loading from "../../../Share/Loading/Loading";
 
 const Login = () => {
+
+  const [email,setEmail] =useState('');
+  const [password,setPassword] =useState('');
+  const [
+      signInWithEmailAndPassword,
+      user,
+      loading,
+      error,
+  ] = useSignInWithEmailAndPassword(auth);
+  const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+  const navigate = useNavigate();
+  const location = useLocation();
+  let errorElement;
+  const from = location.state?.from?.pathname || '/';
+
+  const handelEmailChange = e =>{
+    setEmail(e.target.value);
+}
+
+const handelPasswordChange = e =>{
+    setPassword(e.target.value);
+}
+
+useEffect( () =>{
+  if (user) {
+      navigate(from, {replace: true});
+  }
+},[user])
+
+if (loading || sending) {
+  return <Loading></Loading>
+}
+
+if (error) {
+  errorElement = <p className='text-danger'>Error: {error?.message}</p>
+}
+
+
+const handelLogin = e =>{
+  e.preventDefault();
+  signInWithEmailAndPassword(email,password);
+}
+const resetPassword = async () => {
+        
+  if (email) {
+      await sendPasswordResetEmail(email);
+      toast('Sent email');
+  }
+  else{
+      toast('please enter your email address');
+  }
+}
+
   return (
     <div className="bg-color">
       <div className="text-center ">
         <div className="form-signin form-box-style">
-          <form>
+          <form onSubmit={handelLogin}>
             {/*  <img className="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
              */}
             <h1 className="h2  mb-3 fw-bold">Login </h1>
             <div className="form-floating mb-2">
               <input
                 type="email"
+                onChange={handelEmailChange}
                 className="form-control"
-                id="floatingInput"
                 placeholder="name@example.com"
+                required
               />
               <label for="floatingInput">Email address</label>
             </div>
             <div className="form-floating mb-2">
               <input
-                type="password"className="form-control"placeholder="Password"/>
+                type="password" onChange={handelPasswordChange} className="form-control"placeholder="Password" required/>
               <label for="floatingPassword">Password</label>
             </div>
 
             <div className="checkbox mb-3">
-              <label>
-                <input type="checkbox" value="remember-me" /> Remember me
-              </label>
+              
               <label>
                 <ul>
-                  <a className="text-primary text-decoration-none" href="">Forgot password?</a>
+                  <a className="text-primary text-decoration-none" onClick={resetPassword} href="">Forgot password?</a>
                 </ul>
               </label>
             </div>
+            {errorElement}
             <div>
               <ul>
                 <p> Not a member? <Link to='/signup' className="text-primary text-decoration-none">SignUP Now</Link> </p>
@@ -44,9 +102,9 @@ const Login = () => {
             <button className="w-100 btn btn-lg btn-primary" type="submit">
               Login
             </button>
-            <p className="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
           </form>
         </div>
+        <ToastContainer />
       </div>
     </div>
   );
--- src/Pages/Login/SignUp/SignUp.js
+++ src/Pages/Login/SignUp/SignUp.js
@@ -1,12 +1,68 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import Loading from '../../../Share/Loading/Loading';
 
 const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const [
+      createUserWithEmailAndPassword,
+      user,
+      loading
+    ] = useCreateUserWithEmailAndPassword(auth ,{sendEmailVerification :true});
+
+
+
+  const handleEmailChange = event =>{
+      setEmail(event.target.value);
+  }
+  const handlePasswordChange = event =>{
+      setPassword(event.target.value);
+  }
+  const handleConfirmPasswordChange = event =>{
+      setConfirmPassword(event.target.value);
+  }
+
+  const navigate = useNavigate();
+  const location =useLocation();
+
+  const navigateLogin = () => {
+      navigate('/login');
+  }
+  const from = location.state?.from?.pathname || "/";
+  useEffect(() => {
+      if (user) {
+          navigate(from);
+      }
+  }, [user])
+
+
+  if(loading){
+      return <Loading></Loading>
+  }
+  const handleCreateUser = event =>{
+      event.preventDefault();
+      if(password !== confirmPassword){
+          setError('Your two passwords did not match');
+          return;
+      }
+      if(password.length <6){
+          setError('Password must be 6 characters or longer');
+          return;
+      }
+      
+      createUserWithEmailAndPassword(email, password);
+  }
+
     return (
         <div>
            <div className="text-center">
         <div className="form-signin form-box-style">
-          <form>
+          <form onSubmit={handleCreateUser}>
             {/*  <img className="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
              */}
             <h1 className="h2  mb-3 fw-bold">Sign Up </h1>
@@ -14,8 +70,9 @@ const SignUp = () => {
               <input
                 type="email"
                 className="form-control"
-                id="floatingInput"
+                onChange={handleEmailChange}
                 placeholder="name@example.com"
+                required
               />
               <label for="floatingInput">Email address</label>
             </div>
@@ -23,8 +80,9 @@ const SignUp = () => {
               <input
                 type="password"
                 className="form-control"
-                id="floatingPassword"
+               onChange={handlePasswordChange}
                 placeholder="Password"
+                required
               />
               <label for="floatingPassword">Password</label>
             </div>
@@ -32,16 +90,14 @@ const SignUp = () => {
               <input
                 type="password"
                 className="form-control"
-                id="floatingPassword"
+                onChange={handleConfirmPasswordChange}
                 placeholder="Password"
+                required
               />
               <label for="floatingPassword">Confirm Password</label>
             </div>
 
             <div className="checkbox mb-3">
-              <label>
-                <input type="checkbox" value="remember-me" /> Remember me
-              </label>
               <label>
                 <ul>
                   <a className="text-primary text-decoration-none" as={Link} to="/">
@@ -60,6 +116,7 @@ const SignUp = () => {
             </button>
             <p className="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
           </form>
+          <p style={{color: 'red'}}>{error}</p>
         </div>
       </div>
         </div>
